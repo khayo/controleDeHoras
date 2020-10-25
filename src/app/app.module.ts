1723bbb62e7d571b6b53bdbcf68587d624a24397
khayo <khayovannucci@gmail.com>
2020-10-25 18:07:24 -0300

    Importado modulo de horas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HorasModule } from './horas';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HorasModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
