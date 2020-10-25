e955508cc3470bdc918eda751d2876d85c936beb
khayo <khayovannucci@gmail.com>
2020-10-25 19:26:53 -0300

    Implementado serviço de horas

--- src/app/horas/shared/hora.service.ts
+++ src/app/horas/shared/hora.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
 
+import { Hora } from "./";
+
 @Injectable({
   providedIn: 'root'
 })
 export class HoraService {
 
   constructor() { }
+
+  listarTodos(): Hora[] {
+    const horas = localStorage['horas'];
+    return horas ? JSON.parse(horas) : [];
+  }
+
+  cadastrar(hora: Hora): void {
+    const horas = this.listarTodos();
+    hora.id = new Date().getTime();
+    horas.push(hora);
+    localStorage['horas'] = JSON.stringify(horas);
+  }
+
+  buscaPorId(id: number): Hora {
+    const horas: Hora[] = this.listarTodos();
+    return horas.find(hora => hora.id === id);
+  }
+
+  atualizar(hora: Hora): void {
+    const horas: Hora[] = this.listarTodos();
+    horas.forEach((obj, index, objs) => {
+    if(hora.id === obj.id) {
+      objs[index] = hora;
+    }
+  });
+  localStorage['hora'] = JSON.stringify(horas);
+  }
+
+  remover(id: number): void {
+    let horas: Hora[] = this.listarTodos();
+    horas = horas.filter(hora => hora.id !== id);
+    localStorage['horas'] = JSON.stringify(horas);
+  }
 }
