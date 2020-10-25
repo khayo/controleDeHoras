e0d7dc146dd202205b415aa7b82ffad941dca849
khayo <khayovannucci@gmail.com>
2020-10-25 18:41:04 -0300

    Criado serviço de Horas

--- src/app/horas/horas.module.ts
+++ src/app/horas/horas.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HoraService } from "./shared";
+
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    HoraService
   ]
 })
 export class HorasModule { }
--- src/app/horas/shared/index.ts
+++ src/app/horas/shared/index.ts
@@ -1,3 +1,4 @@
 export * from "./hora.model";
+export * from "./hora.service";
 export * from "./tipoRegistro.enum";
 export * from "./setor.enum";
