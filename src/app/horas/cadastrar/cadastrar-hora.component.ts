d3c78b44bfb114276b77f5607da082b56953ecff
khayo <khayovannucci@gmail.com>
2020-10-31 11:42:26 -0300

    Implementaçao da funçao gerarForm e cadastrar

--- src/app/horas/cadastrar/cadastrar-hora.component.ts
+++ src/app/horas/cadastrar/cadastrar-hora.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Hora, HoraService } from '../shared';
 
 @Component({
   selector: 'app-cadastrar-hora',
@@ -7,8 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarHoraComponent implements OnInit {
 
-  constructor() { }
+  hora: Hora;
+  form: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private horaService: HoraService,
+    private fb: FormBuilder,
+    private router: Router) { }
 
+  ngOnInit() {
+    this.gerarForm();
+  }
+
+  gerarForm() {
+    this.form = this.fb.group({
+      id: ['', []],
+      hora: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      ultimoCliente: ['', []],
+      equipe: ['', []],
+      setor: ['',[]]
+    })
+  }
+
+  cadastrar(){
+    if(this.form.invalid){
+      console.log('Informe todos os dados');
+      return false;
+    }
+    else {
+      this.hora = this.form.value;
+      this.horaService.cadastrar(this.hora);
+      this.router.navigate(["/horas"]);
+    }
+
+
+  }
 }
