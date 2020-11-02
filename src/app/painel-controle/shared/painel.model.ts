646de244b9b6de153bbc01d718a0f61316d2f325
khayo <khayovannucci@gmail.com>
2020-11-02 19:08:16 -0300

    Criado model do painel de controle

---
+++ src/app/painel-controle/shared/painel.model.ts
@@ -0,0 +1,17 @@
+export class Painel {
+    
+    constructor(
+        public horaEntrada?: number,
+        public horaSaida?: number,
+        public tempoAlmoco?: number,
+        public salarioBruto?: number
+    ) {}
+}
+
+/**
+entrada         420     7:00
+saida           1060    17:00
+almoco          72      1:12
+na empresa      640     10:00
+trabalhando     568     8:48
+ */
