a8ee5dd3619f16a22817067987047695e25cbfa3
khayo <khayovannucci@gmail.com>
2020-11-02 19:06:08 -0300

    criado modulo de painel de controle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HorasModule } from './horas';
+import { PainelControleModule } from './painel-controle';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +18,8 @@ import { HorasModule } from './horas';
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
-    HorasModule
+    HorasModule,
+    PainelControleModule
   ],
   providers: [
     StatusBar,
