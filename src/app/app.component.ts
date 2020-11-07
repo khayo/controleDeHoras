e1b62525431ecf13a9206da98a3ba4c5bcd58fb9
khayo <khayovannucci@gmail.com>
2020-11-07 20:51:00 -0300

    Adicionado sidemenu. done #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Lista',
+      url: '/horas/listar',
+      icon: 'list'
+    },
+    {
+      title: 'Painel de controle',
+      url: '/painel',
+      icon: 'settings'
+    }
+  ];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
