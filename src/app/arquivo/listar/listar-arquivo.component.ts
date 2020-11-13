4f4e8f1bdb99f6752418b58d26062feabc660740
khayo <khayovannucci@gmail.com>
2020-11-13 07:32:38 -0300

    listagem de horas arquivadas ok. #18

--- src/app/arquivo/listar/listar-arquivo.component.ts
+++ src/app/arquivo/listar/listar-arquivo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hora, Setor, TipoRegistro } from 'src/app/horas';
+import { ArquivoService } from '../shared';
 
 @Component({
   selector: 'app-listar-arquivo',
@@ -6,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-arquivo.component.scss'],
 })
 export class ListarArquivoComponent implements OnInit {
+  
+  horas: Hora[];
+  tipoRegistro = TipoRegistro;
+  setor = Setor;
 
-  constructor() { }
+  constructor(private arquivoService: ArquivoService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.horas = this.listarTodos();
+  }
+
+  ionViewWillEnter(){
+    this.horas = this.listarTodos();
+  }
+
+  listarTodos(): Hora[]{
+    return this.arquivoService.listarTodos().reverse();
+  }
 
 }
